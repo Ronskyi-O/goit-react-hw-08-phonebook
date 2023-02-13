9db2eef4b80b63d3a03069cb597b57a3de62692d
Oleksandr Ronskyi <oleksandr.ronskyi@gmail.com>
2023-02-13 09:40:13 -0500

    Added page Register

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,5 +1,23 @@
-const Register = () => {
+import { Form, Label } from "./Register.styled"
 
+const Register = () => {
+    return (
+        <Form autocomplete="off">
+            <Label >
+                Username
+                <input type="text" name="name" />
+            </Label>
+            <Label >
+                Email
+                <input type="email" name="email" />
+            </Label>
+            <Label >
+                Password
+                <input type="password" name="password" />
+            </Label>
+            <button type="submit">Register</button>
+        </Form>
+    )
 }
 
 export default Register
