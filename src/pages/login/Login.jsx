2b19ed03987529006a91c055f485a39c62236e4f
Oleksandr Ronskyi <oleksandr.ronskyi@gmail.com>
2023-02-13 09:37:19 -0500

    Added page Login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,19 @@
-const Login = () => {
+import { Form, Label } from "./Login.styled"
 
+const Login = () => {
+    return (
+        <Form utocomplete="off">
+            <Label >
+                Email
+                <input type="email" name="email" />
+            </Label>
+            <Label >
+                Password
+                <input type="password" name="password" />
+            </Label>
+            <button type="submit">Log In</button>
+        </Form>
+    )
 }
 
 export default Login
