1b7b47c0793b332b53451b75ce9681b962b56b27
Oleksandr Ronskyi <oleksandr.ronskyi@gmail.com>
2023-02-12 21:45:35 -0500

    Added component AppBar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,14 @@
 // import { ContactsAddForm } from 'components/ContactsAddForm/ContactsAddForm';
 // import { ContactsFilter } from 'components/ContactsFilter/ContactsFilter';
 // import { ContactsList } from 'components/ContactsList/ContactsList';
+// import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
 
+// const HomePage = lazy(() => import('../pages/Home'));
+// const RegisterPage = lazy(() => import('../pages/Register'));
+// const LoginPage = lazy(() => import('../pages/Login'));
+// const TasksPage = lazy(() => import('../pages/Tasks'));
 
 
 export function App() {
--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,3 +1,13 @@
-export const AppBar = () => {
+import { MainNavigation } from "components/MainNavigation/MainNavigation"
 
+import { Header } from "./AppBar.styled"
+
+
+export const AppBar = () => {
+    return (
+        <Header>
+            <MainNavigation />
+
+        </Header>
+    )
 }
